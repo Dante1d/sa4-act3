67c919e2cefd1aeeac212e327fda269af913ad41
Dante Donaldson <ddonaldsonprime@gmail.com>
2024-02-06 21:56:46 -0600

    Changed guess number check to use a function

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,21 @@
-number = 10
+def guess_number(number):
+    print("I'm thinking of a number...")
+    while True:
+        guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+        
+        if guess.lower() == 'q':
+            print(f"The number was {number}.")
+            break
+        
+        try:
+            guess = int(guess)
+            if guess == number:
+                print("Congratulations! You guessed the right number.")
+                break
+            else:
+                print("Sorry! Try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number or 'q' to quit.")
 
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+# Call the function with the number
+guess_number(10)
