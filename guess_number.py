6f3d4b3582a4860787792a13ba4e73b6ac9878cc
Dante Donaldson <ddonaldsonprime@gmail.com>
2024-02-06 22:06:37 -0600

    Modified code to cap the total number of guesses a user can make and for every wrong guess tell them how many guesses they have left

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,8 @@
-def guess_number(number):
+def guess_number(number, max_guesses):
     print("I'm thinking of a number...")
-    while True:
-        guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+    guesses_left = max_guesses
+    while guesses_left > 0:
+        guess = input(f"What number am I thinking of? You have {guesses_left} guesses left. (Enter 'q' to quit) ")
         
         if guess.lower() == 'q':
             print(f"The number was {number}.")
@@ -12,10 +13,15 @@ def guess_number(number):
             if guess == number:
                 print("Congratulations! You guessed the right number.")
                 break
+            elif guess < number:
+                print("Sorry! Your guess is too low.")
             else:
-                print("Sorry! Try again.")
+                print("Sorry! Your guess is too high.")
+            guesses_left -= 1
         except ValueError:
             print("Invalid input. Please enter a number or 'q' to quit.")
+    else:
+        print(f"Sorry, you've run out of guesses. The number was {number}.")
 
-# Call the function with the number
-guess_number(10)
+# Call the function with the number and maximum number of guesses
+guess_number(10, 5)
